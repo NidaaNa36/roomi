d2a1882bdd5ff23dd6cb4c2f8f781ddacb673fac
NidaaNasserEdin <93448907+NidaaNasserEdin@users.noreply.github.com>
2023-03-04 20:40:47 -0800

    first commit

---
+++ lib/Screens/loginScreen.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Screens/loginScreen2.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  var size, height, width;
+  @override
+  Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+    height = size.height;
+    width = size.width;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Image.asset(
+              'assets/images/66.png',
+              height: height / 3,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Directionality(
+              textDirection: TextDirection.rtl,
+              child: SizedBox(
+                width: width / 1.5,
+                child: const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'البريد الالكتروني',
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Directionality(
+              textDirection: TextDirection.rtl,
+              child: SizedBox(
+                width: width / 1.5,
+                child: const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'كلمة السر',
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            SizedBox(
+              width: width / 1.5,
+              height: height / 15,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LoginScreen2()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(primary: Color(0xFF1C96E8)),
+                child: const Text(
+                  "تسجيل الدخول",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: height / 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "__________________",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  "أو",
+                  style: TextStyle(
+                      color: Colors.grey, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  "__________________",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: height / 10,
+            ),
+            SizedBox(
+              width: width / 1.5,
+              height: height / 15,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(primary: Color(0xFF1C96E8)),
+                child: const Text(
+                  "انشاء حساب جديد",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/loginScreen2.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginScreen2 extends StatefulWidget {
+  const LoginScreen2({super.key});
+
+  @override
+  State<LoginScreen2> createState() => _LoginScreen2State();
+}
+
+class _LoginScreen2State extends State<LoginScreen2> {
+  var size, height, width;
+  @override
+  Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+    height = size.height;
+    width = size.width;
+    return Scaffold(
+      body: Center(
+        child: Column(children: [
+          Image.asset(
+            'assets/images/66.png',
+            height: height / 3,
+          ),
+          const Text(
+            "هل أنت ؟ ",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 21, 82, 122),
+                fontSize: 38),
+          ),
+          Card(
+            margin: const EdgeInsets.all(5),
+            elevation: 8,
+            shadowColor: const Color(0xFF1C96E8),
+            child: Column(children: [
+              Image.asset(
+                'assets/images/s1.png',
+                height: height / 6,
+                width: width / 2,
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  foregroundColor: const Color(0xFF1C96E8),
+                ),
+                child: const Text(
+                  "طالب",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              )
+            ]),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Card(
+            margin: const EdgeInsets.all(5),
+            elevation: 8,
+            shadowColor: const Color(0xFF1C96E8),
+            child: Column(children: [
+              Image.asset(
+                'assets/images/p1.png',
+                height: height / 6,
+                width: width / 2,
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  foregroundColor: const Color(0xFF1C96E8),
+                ),
+                child: const Text(
+                  "مالك سكن",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              )
+            ]),
+          ),
+        ]),
+      ),
+    );
+  }
+}
